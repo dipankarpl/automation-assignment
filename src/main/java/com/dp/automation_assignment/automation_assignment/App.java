0189613c45b95598a2757e9886df8c8240c1eedf
dipankar paul <dipankarp826@yahoo.com>
2024-11-12 00:02:17 +0530

    first commit

---
+++ src/main/java/com/dp/automation_assignment/automation_assignment/App.java
@@ -0,0 +1,10 @@
+package com.dp.automation_assignment.automation_assignment;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
